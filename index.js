d41d4912d1410c3600c772f71b62dda395a3a058
szolarisz <thomas@nyirszikszi.hu>
2022-01-13 11:46:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const port = 4444;
+// const
+// = require(
+
+app.get('/');
+
+app.get('/vonatok');
+
+app.get('/allomasok');
+
+app.post('/ujvonat');
+
+app.post('/deleteVonat');
+
+app.post('/idoModosit');
+
