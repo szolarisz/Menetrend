0ddd194f495fa9405350748f1da2fbf40f8a1c57
szolarisz <thomas@nyirszikszi.hu>
2022-01-13 19:34:17 +0100

    A legördülő lista előállítása ejs és MySQL segítségével.

--- dbModule/dbMuveletek.js
+++ dbModule/dbMuveletek.js
@@ -2,18 +2,41 @@ const mysql = require('mysql');
 const config = require('../config/configDB.json');
 
 const connection = mysql.createConnection({
-    host: config.host, 
-    port: config.port, 
-    database: config.database, 
-    user: config.user, 
+    host: config.host,
+    port: config.port,
+    database: config.database,
+    user: config.user,
     password: config.password
 });
 
-module.exports.vonatLista = function (vId) {
-        myQuery = `SELECT vNev, allId, ora, perc, jelleg FROM esemeny INNER JOIN vonat on vonat.vId=esemeny.vId WHERE vId=${vId}`;
-        connection.query(myQuery, (err, result, fields) => {
-                if (err) throw err;
-                return JSON.parse(JSON.stringify(result));
-            })
+module.exports.vonatEsemenyLista = function (vId) {
+    myQuery = `SELECT vNev, allId, ora, perc, jelleg FROM esemeny INNER JOIN vonat on vonat.vId=esemeny.vId WHERE vId=${vId}`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err) throw err;
+        return JSON.parse(JSON.stringify(result));
+    })
+}
+
+/*
+module.exports.vonatLista = function () {
+    myQuery = `SELECT vId, vNev FROM vonat`;
+    connection.query(myQuery, async (err, result, fields) => {
+        if (err) throw err;
+        console.log(JSON.parse(JSON.stringify(result)))
+        return await Promise.all(JSON.parse(JSON.stringify(result)));
+    })
+}
+*/
+
+module.exports.vonatLista = function (callback) {
+    myQuery = `SELECT vId, vNev FROM vonat`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            console.log(JSON.parse(JSON.stringify(result)))
+            callback(null, JSON.parse(JSON.stringify(result)));
         }
-       
+    })
+}
+
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
 const dbData=require('./dbModule/dbMuveletek.js')
+const path = require('path');
+const { isBuffer } = require('util');
 const port = 4444;
 
 const app =express();
 
-app.use( express.static( 'static',path.join(__dirname,"public")));
+app.use( express.static( path.join(__dirname,'public')));
 
 app.set('view engine', 'ejs');
 // const
 // = require(
 
 app.get('/', (req,res) =>{
-    res.
+    res.sendFile(path.join(__dirname,"/public/index.html"));
 });
 
+app.get('/ejs', (req,res) =>{
+    dbData.vonatLista( (err,data) =>{
+        if (err) throw err;
+        console.log(data)
+        res.render( "index", { vonatok : data});
+    });
+    
+})
+
+/*
 app.get('/vonatok', (req,res) =>{
     
 });
@@ -33,6 +45,11 @@ app.post('/deleteVonat',  (req,res) =>{
 app.post('/idoModosit',  (req,res) =>{
     
 });
+*/
+
+app.get('*', (req,res) =>{
+    res.redirect('/ejs');
+})
 
 app.listen(port);
 
